d316e92fdf308acc7c702b7941b3d4d24fb4839a
Hang <whitewield@126.com>
2017-09-29 14:18:43 -0400

    Hang: override blackJackHand

---
+++ CodeLab2-2017-BlackJack/Assets/Students/hr1051/HR_BlackJackHand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HR_BlackJackHand : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
