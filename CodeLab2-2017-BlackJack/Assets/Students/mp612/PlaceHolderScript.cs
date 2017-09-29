8c4aeec505aaae6eaaff9b5946c4d904cd147822
Matthew Parker <maddparker@gmail.com>
2017-09-28 22:35:41 -0400

    Added Place Holder Script

---
+++ CodeLab2-2017-BlackJack/Assets/Students/mp612/PlaceHolderScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mp612{
+	
+	public class asdadssa : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+
+}
