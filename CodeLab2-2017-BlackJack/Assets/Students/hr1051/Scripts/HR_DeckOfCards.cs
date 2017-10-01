859c1349abcd0fd37fcc15c3ce96da98402ab93f
Hang <whitewield@126.com>
2017-10-01 18:03:07 -0400

    Hang: update DrawCard and AddCardsToDeck

--- CodeLab2-2017-BlackJack/Assets/Students/hr1051/Scripts/HR_DeckOfCards.cs
+++ CodeLab2-2017-BlackJack/Assets/Students/hr1051/Scripts/HR_DeckOfCards.cs
@@ -5,7 +5,27 @@ using Hang;
 
 namespace Hang{
 	public class HR_DeckOfCards : DeckOfCards {
+		[SerializeField] int myDeckCount = 4;
 
+		public override Card DrawCard(){
+			Debug.Log (deck.Cursor);
+			Card nextCard = deck.Next();
+			if (deck.Cursor < 20) {
+				deck.Clear ();
+				AddCardsToDeck ();
+			}
+			return nextCard;
+		}
+
+		protected override void AddCardsToDeck(){
+			foreach (Card.Suit suit in Card.Suit.GetValues(typeof(Card.Suit))){
+				foreach (Card.Type type in Card.Type.GetValues(typeof(Card.Type))){
+					for (int i = 0; i < myDeckCount; i++) {
+						deck.Add (new Card (type, suit));
+					}
+				}
+			}
+		}
 
 	}
 }
