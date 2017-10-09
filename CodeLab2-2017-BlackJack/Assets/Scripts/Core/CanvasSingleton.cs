5aa8b723ce4a9b37f42e78659036310741067e97
DavidFrisch <dmf463@nyu.edu>
2017-10-09 12:45:59 -0400

    added eye lerp and made everything a singleton

---
+++ CodeLab2-2017-BlackJack/Assets/Scripts/Core/CanvasSingleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasSingleton : MonoBehaviour {
+
+    public static CanvasSingleton instance;
+
+	// Use this for initialization
+	void Start () {
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else Destroy(gameObject);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ CodeLab2-2017-BlackJack/Assets/Scripts/Core/EdgeLerp.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EdgeLerp : MonoBehaviour
+{
+
+    private Image blurredEye;
+    private bool hasFaded;
+    Vector3 initialScale;
+    Vector3 targetScale;
+    bool faded;
+    float duration = 1;
+    float timeElapsed = 0;
+    public static EdgeLerp instance;
+
+    // Use this for initialization
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        blurredEye = this.GetComponent<Image>();
+        blurredEye.CrossFadeAlpha(0.75f, 0, true);
+        initialScale = blurredEye.transform.localScale;
+        targetScale = new Vector3(initialScale.x + 0.4f, initialScale.y + 0.4f, initialScale.z + 0.4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!faded)
+        {
+            timeElapsed = Mathf.Min(duration, timeElapsed + Time.deltaTime);
+            blurredEye.transform.localScale = Vector3.Lerp(initialScale, targetScale, timeElapsed / duration);
+            if (timeElapsed == duration)
+            {
+                faded = true;
+                timeElapsed = 0;
+            }
+        }
+        else
+        {
+            timeElapsed = Mathf.Min(duration, timeElapsed + Time.deltaTime);
+            blurredEye.transform.localScale = Vector3.Lerp(targetScale, initialScale, timeElapsed / duration);
+            if (timeElapsed == duration)
+            {
+                faded = false;
+                timeElapsed = 0;
+            }
+        }
+
+
+
+    }
+}
