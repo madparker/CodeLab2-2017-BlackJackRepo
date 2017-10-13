f390156d01f5eb89ce07c575d051a91e15720675
Andrew Petersell <andrewpetersell@gmail.com>
2017-10-12 20:23:16 -0400

    Getting Pose From Bag Working

---
+++ CodeLab2-2017-BlackJack/Assets/Scripts/PoseyMatchy/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class GameManager : MonoBehaviour {
+
+	public Sprite [] poses;
+	public static ShuffleBag<Sprite> poseBag; 
+	public Sprite currentPose;
+	public float currentTimer;
+	public float maxTimer;
+
+	public Image player;
+	public Image [] bandMates;
+	public Image choreographer; 
+
+
+	void Awake () 
+	{
+		if (!IsValidBag())  
+		{
+			poseBag = new ShuffleBag<Sprite> (); 
+			for (int i = 0; i < poses.Length; i++) 
+			{
+				poseBag.Add (poses [i]);
+			}
+		}
+	}
+		
+	void Start () 
+	{
+		player = GameObject.Find ("PlayerIcon").GetComponent<Image> ();
+		choreographer = GameObject.Find ("Choreographer").GetComponent<Image> ();
+		for (int i = 0; i < bandMates.Length; i++) 
+		{
+			bandMates[i] = GameObject.Find("BandMate_" + i).GetComponent<Image>();
+		}
+	}
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			GetNewPose ();
+		}
+		choreographer.sprite = currentPose;
+		for (int i = 0; i < bandMates.Length; i++) 
+		{
+			bandMates [i].sprite = currentPose;
+		}
+	}
+
+	bool IsValidBag()
+	{
+		return poseBag != null; 
+	}
+
+	void GetNewPose()
+	{
+		currentPose = poseBag.Next ();
+	}
+}
